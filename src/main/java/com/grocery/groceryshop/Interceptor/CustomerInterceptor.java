28571d11ebfba2f117d526a0bf2114609d9be4d0
lishunli <505437592@qq.com>
2019-09-26 14:46:11 +0800

    init

--- src/main/java/com/grocery/groceryshop/Interceptor/CustomerInterceptor.java
+++ src/main/java/com/grocery/groceryshop/Interceptor/CustomerInterceptor.java
@@ -12,6 +12,7 @@ public class CustomerInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
+    log.info("CustomerInterceptor.preHandle--------------");
     return true;
   }
 
@@ -21,10 +22,15 @@ public class CustomerInterceptor implements HandlerInterceptor {
       HttpServletResponse response,
       Object handler,
       ModelAndView modelAndView)
-      throws Exception {}
+      throws Exception {
+
+    log.info("CustomerInterceptor.postHandle------------");
+  }
 
   @Override
   public void afterCompletion(
       HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-      throws Exception {}
+      throws Exception {
+    log.info("CustomerInterceptor.afterCompletion---------------");
+  }
 }
--- src/main/java/com/grocery/groceryshop/filter/CustomerFilter.java
+++ src/main/java/com/grocery/groceryshop/filter/CustomerFilter.java
@@ -11,16 +11,21 @@ import java.io.IOException;
 @Slf4j
 public class CustomerFilter implements Filter {
   @Override
-  public void init(FilterConfig filterConfig) throws ServletException {}
+  public void init(FilterConfig filterConfig) throws ServletException {
+    log.info("customerFilter  init ................");
+  }
 
   @Override
   public void doFilter(
       ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
       throws IOException, ServletException {
     HttpServletRequest request = (HttpServletRequest) servletRequest;
+    log.info("customerFilter  doFilter ................");
     filterChain.doFilter(request, servletResponse);
   }
 
   @Override
-  public void destroy() {}
+  public void destroy() {
+    log.info("customerFilter  destroy ................");
+  }
 }
