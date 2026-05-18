4fae2c457ac38d74d89bcb7e7527fa4d0b9eedf0
luotuoxiangzi <505437592@qq.com>
2026-05-18 21:57:11 +0800

    chore: 排除 DataSourceAutoConfiguration 自动配置
    项目使用 Druid 手动配置数据源，排除 Spring Boot 默认 JDBC 自动配置以避免冲突。

--- src/main/java/com/grocery/groceryshop/GroceryShopApplication.java
+++ src/main/java/com/grocery/groceryshop/GroceryShopApplication.java
@@ -2,10 +2,11 @@ package com.grocery.groceryshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GroceryShopApplication {
 
   public static void main(String[] args) {
